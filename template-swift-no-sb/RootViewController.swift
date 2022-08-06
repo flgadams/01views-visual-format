34a43c00badc2f2d10fc153ea9078209038fe3c3
Glenn Adams <flgadams@gmail.com>
2022-08-06 12:29:36 -0400

    Added 5 buttons

--- template-swift-no-sb/RootViewController.swift
+++ template-swift-no-sb/RootViewController.swift
@@ -11,19 +11,87 @@ import UIKit
 
 class RootViewController: UIViewController {
 
-    lazy var oneButton:UIButton = {  //snip zxuibtnl
+    lazy var aButton:UIButton = {  //snip zxuibtnl
         let ui = UIButton(type: .system)
-        ui.setTitle("OneB", for: .normal)
+        ui.setTitle("A", for: .normal)
         ui.titleLabel?.font = .systemFont(ofSize: 20)
         ui.setTitleColor(UIColor.white, for: UIControl.State.normal)
         ui.backgroundColor = UIColor.orange
         ui.layer.borderWidth = 1
         ui.layer.cornerRadius = 16
-        ui.addTarget(self, action: #selector(oneButtonTouchSelector), for: .touchUpInside)
+        ui.addTarget(self, action: #selector(aButtonTouchSelector), for: .touchUpInside)
         return ui
     }()
     
-    @objc fileprivate func oneButtonTouchSelector(sender:Any) {
+    @objc fileprivate func aButtonTouchSelector(sender:Any) {
+        let file = "\(#file)".components(separatedBy: "/").last!; NSLog("\n\u{2705} \(#function) Line \(#line) of \(file)\n")
+        let vc = UIViewController()
+        vc.view.backgroundColor = .purple
+        present(vc, animated: true)
+    }
+    
+    lazy var bButton:UIButton = {  //snip zxuibtnl
+        let ui = UIButton(type: .system)
+        ui.setTitle("B", for: .normal)
+        ui.titleLabel?.font = .systemFont(ofSize: 20)
+        ui.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        ui.backgroundColor = UIColor.orange
+        ui.layer.borderWidth = 1
+        ui.layer.cornerRadius = 16
+        ui.addTarget(self, action: #selector(bButtonTouchSelector), for: .touchUpInside)
+        return ui
+    }()
+    
+    @objc fileprivate func bButtonTouchSelector(sender:Any) {
+        let file = "\(#file)".components(separatedBy: "/").last!; NSLog("\n\u{2705} \(#function) Line \(#line) of \(file)\n")
+        
+    }
+    lazy var cButton:UIButton = {  //snip zxuibtnl
+        let ui = UIButton(type: .system)
+        ui.setTitle("C", for: .normal)
+        ui.titleLabel?.font = .systemFont(ofSize: 20)
+        ui.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        ui.backgroundColor = UIColor.orange
+        ui.layer.borderWidth = 1
+        ui.layer.cornerRadius = 16
+        ui.addTarget(self, action: #selector(cButtonTouchSelector), for: .touchUpInside)
+        return ui
+    }()
+    
+    @objc fileprivate func cButtonTouchSelector(sender:Any) {
+        let file = "\(#file)".components(separatedBy: "/").last!; NSLog("\n\u{2705} \(#function) Line \(#line) of \(file)\n")
+        
+    }
+    
+    lazy var dButton:UIButton = {  //snip zxuibtnl
+        let ui = UIButton(type: .system)
+        ui.setTitle("D", for: .normal)
+        ui.titleLabel?.font = .systemFont(ofSize: 20)
+        ui.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        ui.backgroundColor = UIColor.orange
+        ui.layer.borderWidth = 1
+        ui.layer.cornerRadius = 16
+        ui.addTarget(self, action: #selector(dButtonTouchSelector), for: .touchUpInside)
+        return ui
+    }()
+    
+    @objc fileprivate func dButtonTouchSelector(sender:Any) {
+        let file = "\(#file)".components(separatedBy: "/").last!; NSLog("\n\u{2705} \(#function) Line \(#line) of \(file)\n")
+        
+    }
+    lazy var eButton:UIButton = {  //snip zxuibtnl
+        let ui = UIButton(type: .system)
+        ui.setTitle("E", for: .normal)
+        ui.titleLabel?.font = .systemFont(ofSize: 20)
+        ui.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        ui.backgroundColor = UIColor.orange
+        ui.layer.borderWidth = 1
+        ui.layer.cornerRadius = 16
+        ui.addTarget(self, action: #selector(eButtonTouchSelector), for: .touchUpInside)
+        return ui
+    }()
+    
+    @objc fileprivate func eButtonTouchSelector(sender:Any) {
         let file = "\(#file)".components(separatedBy: "/").last!; NSLog("\n\u{2705} \(#function) Line \(#line) of \(file)\n")
         
     }
@@ -32,19 +100,8 @@ class RootViewController: UIViewController {
         self.view.backgroundColor = .systemGray
         
         var constraintsArr=[NSLayoutConstraint]()
-        let v1 = oneButton
-        v1.setTitle("ONE", for: .normal)
-        v1.backgroundColor = .red
         
-        let v2 = UIButton()
-        v2.setTitle("TWO", for: .normal)
-        v2.backgroundColor = .green
-        
-        let v3 = UIButton()
-        v3.setTitle("THREE", for: .normal)
-        v3.backgroundColor = .yellow
-        
-        let viewArr = [v1,v2, v3]
+        let viewArr = [aButton, bButton, cButton, dButton, eButton]
         var viewDict=Dictionary<String,UIView>()
         var vFormat = "V:|"
         var hFormat = ""
@@ -52,8 +109,8 @@ class RootViewController: UIViewController {
         for (i,eachView) in viewArr.enumerated() {
             view.addSubview(eachView)
             eachView.translatesAutoresizingMaskIntoConstraints=false
-            vFormat = vFormat + "-25-[v\(i)(75)]"
-            hFormat =  "H:|-[v\(i)]-|"
+            vFormat = vFormat + "-25-[v\(i)(75)]" //25 gap between buttons
+            hFormat =  "H:|-[v\(i)]-|" //8 from edges
             let h = NSLayoutConstraint.constraints(withVisualFormat: hFormat, metrics: nil, views: ["v\(i)":eachView])
             constraintsArr.append(contentsOf: h)
             viewDict["v\(i)"] = eachView
